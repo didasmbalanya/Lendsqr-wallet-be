79be9298b59309bffd005a242478897da4fc4935
Didas Mbalanya <didasmbalanya@gmail.com>
2025-07-11 16:31:48 +0300

    set app and server

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,23 @@
 import express from "express";
+import { setupRoutes } from "./routes";
+import knex from "knex";
+import { ModelFactory } from "./models";
 
+// Initialize DB
+
+let environment = process.env.NODE_ENV || "development";
+const db = knex(require("../knexfile")[environment]);
+
+// Initialize models
+const modelFactory = new ModelFactory(db);
+
+// Create Express app
 const app = express();
 
+// Middleware
+app.use(express.json());
 
-export default app;
+// Set up routes
+setupRoutes(app, modelFactory, db);
+
+export { app };
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
-import app from './app';
+import "./types/express.types"
+import { app } from "./app";
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
